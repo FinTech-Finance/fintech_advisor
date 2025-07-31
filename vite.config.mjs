600bd1aac4e0fe330f596cb6cd52eebd795ee889
ThinkSharp <utkarshaster@gmail.com>
2025-07-31 18:24:22 +0530

    Fix Vite config for GitHub Pages deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,12 +3,10 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  base: "/fintech_advisor/", // ✅ Add this line!
   build: {
-    outDir: "build",
+    // outDir: "build", // ❌ remove or comment this
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
